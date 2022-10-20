25f40bb65df643059e6c828685009a0e80fea96e
Ogonna <Goddeyalaba11@gmail.com>
2022-10-20 12:19:33 -0700

    ok

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,6 @@
-#include "maim.h"
+#include "main.h"
 /**
- * print_numbers - print 0123456789.
+ * print_numbers - print 0123456789
  * Return: 0
  */
 void print_numbers(void)
@@ -14,3 +14,4 @@ void print_numbers(void)
 	}
 _putchar('\n');
 }
+
