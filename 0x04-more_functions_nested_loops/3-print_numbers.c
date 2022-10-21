140157370d324716ec266208fb0f8e174b76bf08
Ogonna <Goddeyalaba11@gmail.com>
2022-10-21 00:34:55 -0700

    ok

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,12 +8,20 @@
 
 void print_numbers(void)
 {
-	char c;
+	int a, i, j = 1, x = 0;
 
-	for (c = '0'; c <= '9'; c++)
-	{
+	printf("Input number of lines: ");
+	scanf("%d", &a);
+	for (i = 1; i <= a; i++)
+		{
+		while (x < 9)
+		{
+			printf("%d ", j++);
+			x++;
+		}
+		x = 0;
+		printf("\n");
+		}
 
-		_putchar(c);
-	}
-		_putchar('\n');
+	return (0);
 }
