f35f0826e17bc1308462e1873a6c330b3a14caa7
Ogonna <Goddeyalaba11@gmail.com>
2022-11-02 07:20:19 -0700

    ok

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion- returns the length of string
+ *
+ * @s: the string to check the length of s
+ *
+ * Return: an integer that displays the length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _stlen_recursion(s + 1));
+
+		return (0);
+	}
+}
+
